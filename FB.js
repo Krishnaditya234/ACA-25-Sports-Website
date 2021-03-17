2cf2bea61e5c3a6f0e2f4b709ac533dc50e89a68
Krishnaditya234 <73075473+Krishnaditya234@users.noreply.github.com>
2021-03-17 22:48:34 +0530

    Add files via upload

---
+++ FB.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDek88pvTaqyuRFNxa-6pbnPCAYSrpiYKk",
+    authDomain: "ipl-result-72f06.firebaseapp.com",
+    databaseURL: "https://ipl-result-72f06-default-rtdb.firebaseio.com",
+    projectId: "ipl-result-72f06",
+    storageBucket: "ipl-result-72f06.appspot.com",
+    messagingSenderId: "35794132680",
+    appId: "1:35794132680:web:7a3780861e835fa4000ffd"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
